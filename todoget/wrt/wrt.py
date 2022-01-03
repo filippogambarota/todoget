d1c1278182a71129537c8597f6b26743cf1ea038
Filippo Gambarota <filippo.gambarota@gmail.com>
2022-01-03 19:52:38 +0100

    creating header instead of yaml

--- todoget/wrt/wrt.py
+++ todoget/wrt/wrt.py
@@ -1,5 +1,7 @@
 from datetime import date, datetime
 
+from todoget.utils import utils
+
 def create_header():
     title = "Todoget"
     now = datetime.now()
@@ -8,13 +10,24 @@ def create_header():
     header = '---' + '\n' + 'title: ' + title + '\n' + 'date: ' + str(date.today()) + ' ' + '(' + current_time  + ')' + '\n' + '---' + '\n'
     return header
 
+def create_out_title():
+    title =  "Todoget Report"
+    now = datetime.now()
+    current_time = now.strftime("%H:%M")
+    
+    out_title = '# ' + title + '\n' + 'last update: ' + str(date.today()) + ' ' + '(' + current_time  + ')' + '\n'
+    
+    return out_title
+    
 def create_file_title(filename):
-    title = '\n' + "#" + " " + filename + '\n'
+    title = '\n' + "##" + " " + filename + '\n'
     return title
 	
-def format_line(line, number):
-	line_new = '- ' + "Line " + str(number) + ":" + ' ' +line
-	return line_new
+def format_line(line, number, pattern):
+    line = utils.remove_comments_sym(line)
+    line = utils.remove_pattern_sym(line, pattern)
+    line_new = '- ' + "Line " + str(number) + ":" + ' ' + line
+    return line_new
 
 def rep_string(string, times, sep = ""):
 	out = [string]
